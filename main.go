ad78115797d026927981b97e4752fdee0fd721ae
sinmetal <sinmetal@sinmetalcraft.jp>
2020-10-07 22:22:15 +0900

    add build config
    GitHub Container Registry(ghcr.io) への Push を目指す

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sinmetal/gcptoolbox/cmd"
+)
+
+func main() {
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+	os.Exit(0)
+}
