aaa0aea3d79c9aa1cb5b618f5731ccc3ec998a39
sinmetal <sinmetal@sinmetalcraft.jp>
2025-05-14 19:43:56 +0900

    add debug log

--- executioner/handler.go
+++ executioner/handler.go
@@ -154,6 +154,7 @@ func (h *Handler) HandleDeletePreparation(ctx context.Context, w http.ResponseWr
 			StatusCode: http.StatusOK,
 		}
 	}
+	fmt.Printf("create backup ope %s projects/%s/instances/%s/databases/%s", ope.Name(), req.ProjectID, req.InstanceID, req.DatabaseID)
 
 	task := &cloudtasksbox.JsonPostTask{
 		Audience:     h.cloudRunURI,
